413fec51192145fb56e00948f38ddff0178b782c
areebakhund25 <areebakhund25@gmail.com>
2026-04-24 01:18:49 +0500

    Set debug mode to False in app.run
    Disable debug mode for production environment.

--- app.py
+++ app.py
@@ -47,4 +47,4 @@ import os
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))  # Use Render's port or default 5000
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=port, debug=False)
